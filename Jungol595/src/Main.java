d483c25e80cd7c465f9ea5526acbbf9986c1eb61
kyung186 <89821357+kyung186@users.noreply.github.com>
2021-08-31 11:08:41 +0900

    Jungol595

---
+++ Jungol595/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+
+		String str = "Hing Gil Dong";
+
+		for (int i = 3; i <= 6; i++) {
+			System.out.print(str.charAt(i));
+		}
+	}
+}
