209236b127161150d02b1c010640a9911c910d11
kyung186 <89821357+kyung186@users.noreply.github.com>
2021-08-31 11:11:57 +0900

    Update Main.java

--- Jungol595/src/Main.java
+++ Jungol595/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 
-		String str = "Hing Gil Dong";
+		String str = "Hong Gil Dong";
 
 		for (int i = 3; i <= 6; i++) {
 			System.out.print(str.charAt(i));
